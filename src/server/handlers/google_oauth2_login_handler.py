a340d142231b64d048b8dbb63b8418176b07d1da
davidl <davidvoler@gmail.com>
2014-10-08 15:55:07 +0300

    logging to solve google

--- src/server/handlers/google_oauth2_login_handler.py
+++ src/server/handlers/google_oauth2_login_handler.py
@@ -25,8 +25,7 @@ class GoogleOAuth2LoginHandler(RequestHandler,
             logging.info('probably call by google')
             user = yield self.get_authenticated_user(
                 google_oauth2_redirect_uri,
-                code=self.get_argument('code'),
-                extra_fields=['email'])
+                code=self.get_argument('code'))
             #response =  yield http_client.fetch('https://www.googleapis.com/oauth2/v1/userinfo?access_token='+access_token)
             #user = json.loads(response.body)
             # Save the user with e.g. set_secure_cookie
