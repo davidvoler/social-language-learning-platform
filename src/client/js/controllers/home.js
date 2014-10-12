1574c50b6b35161e63bd8c402369e0b3ba8b4aa8
davidl <davidvoler@gmail.com>
2014-10-12 13:32:59 +0300

    removed xhrf

--- src/client/js/controllers/home.js
+++ src/client/js/controllers/home.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('sllp.app')
-  .controller('HomeController', ['$scope','Lesson','Language','$cookieStore',
-    function ($scope,Lesson,Language,$cookieStore) {
+  .controller('HomeController', ['$scope','Lesson','Language',
+    function ($scope,Lesson,Language) {
       $scope.controllerName = 'HomeController';
       $scope.lessons = Lesson.query();
       $scope.languages = Language.languages;
       $scope.languageChange = function(language){
       };
-      $scope.user = $cookieStore.get('sllp_user');
+      //$scope.user = $cookieStore.get('sllp_user');
       $scope.exercise = {};
     }
   ]
