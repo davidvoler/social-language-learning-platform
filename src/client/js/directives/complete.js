ea4d53814c71775b90ca736f27990f9c661cfc24
davidlevy <davidvoler@yahoo.com>
2014-10-26 08:39:04 +0200

    adding utils with shuffle array Adding practiceComplete directive.

--- src/client/js/directives/complete.js
+++ src/client/js/directives/complete.js
@@ -83,4 +83,54 @@ angular.module('sllp.app')
                 '</span>'+
                 '</div>'
     };
+  })
+  .directive('practiceComplete', function () {
+    return {
+      restrict: 'E',
+      //scope: { exercise: '='},
+      controller : function($scope) {
+        $scope.init =function(){
+          //shuffle exercise options
+          for (var i = 0;i<$scope.exercise.items){
+            if ($scope.exercise.items[i].type=='options'){
+                $scope.exercise.items[i].options = Shuffle($scope.exercise.items[i].options);
+            }
+          }
+        };
+        //set user's mark for the exercise
+        $scope.setMark = function(){
+           //call the $scope.setUserMark();
+        };
+
+        $scope.checkCorrect = function(item,id){
+            for (var i = 0;i<item.options.length;i++){
+                if (item.options[i].correct){
+                    if(item.user_selection == item.options[i].val){
+                        //user selected a correct answer
+                        console.log('correct');
+                        $scope.setExerciseResults(id,1);
+                        return;
+                    }
+                }
+            }
+            //user selected a wrong answer
+            $scope.setExerciseResults(id,-1)
+            console.log('wrong answer');
+
+        };
+      },
+
+      template:
+                '<div>'+
+                '<span  ng-repeat="item in exercise.items">'+
+                  '<span ng-if="item.type==\'txt\'">{{item.val}}</span>'+
+                  '<select ng-change="checkCorrect(item, exercise.id)" ng-model="item.user_selection" ng-if="item.type==\'select\'">'+
+                    '<option></option>'+
+                    '<option ng-repeat="option in item.options">{{option.val}}</option>'+
+                  '</select>'+
+                '</span>'+
+                '</div>'
+    };
   });
+
+
