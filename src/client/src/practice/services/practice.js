f274bd5ebf64e6e84a3d26c98473913cea1eba95
davidl <davidvoler@gmail.com>
2014-11-06 07:34:14 +0200

    basic match functionality

---
+++ src/client/src/practice/services/practice.js
@@ -0,0 +1,18 @@
+(function () {
+
+  function Practice($http) {
+
+    var self = this;
+
+    self.find = function () {
+
+    };
+    self.saveMark = function () {
+
+    };
+
+  }
+
+  angular.angular.module('sllp.practice')
+    .service('Practice', Practice)
+}());
