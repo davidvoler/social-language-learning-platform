d31e9927ccfd18732940040bb4c36ac61618130e
davidl <davidl@crow.co.il>
2015-06-02 13:43:00 +0300

    starting with material design

--- src/exercise/web/vocabulary/edit.js
+++ src/exercise/web/vocabulary/edit.js
@@ -9,27 +9,27 @@
     return {
       restrict: 'E',
       scope: {exercise: "="},
-      controller: function ($scope) {
-        $scope.addItem = function () {
-          $scope.exercise.items.push({term: '', translation: '', translit: ''});
+      link: function (scope, element, attr) {
+        scope.addItem = function () {
+          scope.exercise.items.push({term: '', translation: '', translit: ''});
         };
-        $scope.init = function () {
-          if (!$scope.exercise.items) {
-            $scope.exercise.items = [];
+        scope.init = function () {
+          if (!scope.exercise.items) {
+            scope.exercise.items = [];
           }
-          if ($scope.exercise.items.length < 4) {
+          if (scope.exercise.items.length < 4) {
             //add 4 empty items
-            for (var i = $scope.exercise.items.length; i < 4; i++) {
-              $scope.addItem();
+            for (var i = scope.exercise.items.length; i < 4; i++) {
+              scope.addItem();
             }
           }
         };
-        $scope.init();
+        scope.init();
       },
       templateUrl: '/static/src/exercise/web/vocabulary/edit.html'
     }
   }
 
   angular.module('sllp.exercise')
-    .directive('slEditVocabulary', [slEditVocabulary])
+      .directive('slEditVocabulary', [slEditVocabulary])
 }());
--- src/exercise/web/vocabulary/preview.js
+++ src/exercise/web/vocabulary/preview.js
@@ -7,20 +7,20 @@
 
     return {
       restrict: 'E',
-      //scope: { exercise: '='},
+      scope: { exercise: '='},
       //templateUrl: '/static/partials/directives/vocabulary/preview.html'
-      controller: function ($scope) {
-        $scope.currentItem = 0;
-        $scope.selectedItems = [];
-        $scope.checkCorrect = function (idx, translation, id) {
-          if (translation == $scope.exercise.items[$scope.currentItem].translation) {
-            $scope.selectedItems.push({value: translation, correct: true});
-            $scope.setExerciseResults(id, 1);
+      link: function (scope, element, attr) {
+        scope.currentItem = 0;
+        scope.selectedItems = [];
+        scope.checkCorrect = function (idx, translation, id) {
+          if (translation == scope.exercise.items[scope.currentItem].translation) {
+            scope.selectedItems.push({value: translation, correct: true});
+            scope.setExerciseResults(id, 1);
           } else {
-            $scope.selectedItems.push({value: translation, correct: false});
-            $scope.setExerciseResults(id, -1);
+            scope.selectedItems.push({value: translation, correct: false});
+            scope.setExerciseResults(id, -1);
           }
-          $scope.currentItem++;
+          scope.currentItem++;
         }
       },
       templateUrl: '/static/src/exercise/web/vocabulary/preview.html'
