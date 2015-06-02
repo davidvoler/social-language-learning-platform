4849e1d52d9244ed5f7e0cf17d907ef117b075c3
davidl <davidl@crow.co.il>
2015-06-02 10:13:30 +0300

    Design of editor

--- src/editor/web/edit/edit.js
+++ src/editor/web/edit/edit.js
@@ -1,84 +1,96 @@
 (function () {
-    "use strict";
-    function EditorEditController(EditorService) {
-        var self = this;
-        self.lesson = {};
+  "use strict";
+  function EditorEditController(EditorService) {
+    var self = this;
+    self.lesson = {};
 
-        self.exerciseTypes = [
-            {typeName:'complete',icon:'pencil-box-outline'},
-            {typeName:'match',icon:'view-quilt'},
-            {typeName:'mix',icon:'arrange-send-to-back'},
-            {typeName:'question',icon:'help'},
-            {typeName:'text',icon:'tooltip-text'},
-            {typeName:'video',icon:'video'},
-            {typeName:'vocabulary',icon:'briefcase'}
-        ];
-        self.getClass = function(e){
-          return 'mdi-'+ e.icon;
-        };
-        self.exercises = [
-            {
-                name: 'First steps in Georgian',
-                type: 'Min Li Chan',
-                level: 1,
-                weight:0,
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                name: 'Numbers in Georgian',
-                type: 'Min Li Chan',
-                level: 1, weight:1,
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                name: 'Meet and Greet',
-                type: 'Min Li Chan',
-                level: 1, weight:2,
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                name: 'Dinner',
-                type: 'Min Li Chan',
-                level: 1, weight:3,
-                notes: " I'll be in your neighborhood doing errands"
-            },
-            {
-                name: 'Friends',
-                type: 'Min Li Chan',
-                level: 1, weight:4,
-                notes: " I'll be in your neighborhood doing errands"
-            }
-        ];
-        self.selectedExercise = {};
-        self.moveUp = function (idx) {
-            if (idx <= 0) {
-                return;
-            } else {
-                var elm = self.exercises[idx];
-                self.exercises.splice(idx, 1);
-                self.exercises.splice(idx - 1, 0, elm);
-            }
-            self.save();
-        };
+    self.exerciseTypes = [
+      {etype: 'complete', icon: 'pencil-box-outline'},
+      {etype: 'match', icon: 'view-quilt'},
+      {etype: 'mix', icon: 'arrange-send-to-back'},
+      {etype: 'question', icon: 'help'},
+      {etype: 'text', icon: 'tooltip-text'},
+      {etype: 'video', icon: 'video'},
+      {etype: 'vocabulary', icon: 'briefcase'}
+    ];
+    self.getClass = function (e) {
+      return 'mdi-' + e.icon;
+    };
+    self.exercises = [
 
-        self.moveDown = function (idx) {
-            if (idx >= self.exercises.length - 1) {
-                return;
-            } else {
-                var elm = self.exercises[idx];
-                self.exercises.splice(idx, 1);
-                self.exercises.splice(idx + 1, 0, elm);
-            }
-            self.save();
-        };
-        self.save = function(){
-            return true;
-        };
-        self.selectExercise = function(idx){
-            self.selectedExercise = self.exercises[idx];
-        };
-    }
+      {
+        name: 'Meet and Greet',
+        etype: 'complete',
+        level: 1, weight: 2,
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        name: 'Dinner',
+        etype: 'complete',
+        level: 1, weight: 3,
+        notes: " I'll be in your neighborhood doing errands"
+      },
+      {
+        name: 'Friends',
+        etype: 'video',
+        level: 1, weight: 4,
+        notes: " I'll be in your neighborhood doing errands"
+      }
+    ];
+    self.selectedExercise = {};
+    self.moveUp = function (idx) {
+      if (idx <= 0) {
+        return;
+      } else {
+        var elm = self.exercises[idx];
+        self.exercises.splice(idx, 1);
+        self.exercises.splice(idx - 1, 0, elm);
+      }
+      self.save();
+    };
 
-    angular.module('sllp.editor')
-        .controller('EditorEditController', ['EditorService', EditorEditController]);
+    self.moveDown = function (idx) {
+      if (idx >= self.exercises.length - 1) {
+        return;
+      } else {
+        var elm = self.exercises[idx];
+        self.exercises.splice(idx, 1);
+        self.exercises.splice(idx + 1, 0, elm);
+      }
+      self.save();
+    };
+    self.save = function () {
+      return true;
+    };
+    self.selectExercise = function (idx) {
+      self.selectedExercise = self.exercises[idx];
+    };
+
+    self.createExercise = function(etype){
+      return {
+        etype:etype.etype,
+        data:{},
+        name:'New Exercise'
+      };
+    };
+    self.addExercise = function(etype){
+      self.exercises.push(self.createExercise(etype));
+      self.selectExercise(self.exercises.length -1);
+    };
+    self.getExerciseClass = function(e){
+      for (var i in self.exerciseTypes){
+        if (self.exerciseTypes[i].etype == e.etype){
+          return self.getClass(self.exerciseTypes[i]);
+        }
+      }
+      return 'mdi-dots-vertical';
+    };
+    self.deleteExercise = function(idx){
+      self.exercises.splice(idx, 1);
+    };
+
+  }
+
+  angular.module('sllp.editor')
+      .controller('EditorEditController', ['EditorService', EditorEditController]);
 }());
