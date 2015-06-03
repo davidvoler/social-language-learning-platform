da3d0834016e42f2134f036cbb32fba125922077
davidl <davidl@crow.co.il>
2015-06-03 18:19:13 +0300

    match edit

--- src/exercise/web/match/edit.js
+++ src/exercise/web/match/edit.js
@@ -9,29 +9,29 @@
     return {
       restrict: 'E',
       scope: {exercise: "="},
-      controller: function ($scope) {
-        $scope.addItem = function () {
-          $scope.exercise.items.push({part1: '', part2: ''});
+      link: function (scope, element, attr) {
+        scope.addItem = function () {
+          scope.exercise.items.push({part1: '', part2: ''});
         };
-        $scope.init = function () {
-          if (!$scope.exercise.items) {
-            $scope.exercise.items = [];
+        scope.init = function () {
+          if (!scope.exercise.items) {
+            scope.exercise.items = [];
           }
-          if ($scope.exercise.items.length < 4) {
+          if (scope.exercise.items.length < 4) {
             //add 4 empty items
-            for (var i = $scope.exercise.items.length; i < 4; i++) {
-              $scope.addItem();
+            for (var i = scope.exercise.items.length; i < 4; i++) {
+              scope.addItem();
             }
           }
         };
-        $scope.init();
+        scope.init();
 
       },
 
-      templateUrl: '/static/src/exercise_directives/match/edit.html'
+      templateUrl: '/static/src/exercise/web/match/edit.html'
     }
   }
 
-  angular.module('sllp.exercise_directives')
+  angular.module('sllp.exercise')
     .directive('slEditMatch', [slEditMatch])
 }());
--- src/exercise/web/match/preview.js
+++ src/exercise/web/match/preview.js
@@ -1,16 +1,19 @@
 (function () {
   /**
-   * slPreviewDirname Directive
+   * slPreviewMatch Directive
    * The preview version
    */
-  function slPreviewDirname() {
+  function slPreviewMatch() {
 
     return {
-
-      templateUrl:'/static/src/exercise_directives/sdir/preview.html'
+      restrict: 'E',
+      scope: {exercise: "="},
+      link: function (scope, element, attr) {
+      },
+      templateUrl:'/static/src/exercise/web/match/preview.html'
     };
   }
 
-  angular.module('sllp.exercise_directives')
-    .directive('slPreviewDirname',[slPreviewDirname]);
+  angular.module('sllp.exercise')
+    .directive('slPreviewMatch',[slPreviewMatch]);
 }());
